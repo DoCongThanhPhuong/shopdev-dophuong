0e37475cc0f65315e3f007e838a93cbec4d8d67b
DoCongThanhPhuong <bido23082003@gmail.com>
2024-05-30 10:04:57 +0700

    Initialize Project

---
+++ src/ui/ModalBase.jsx
@@ -0,0 +1,31 @@
+// components
+import Grow from '@mui/material/Grow'
+import Modal from '@mui/material/Modal'
+
+// utils
+import PropTypes from 'prop-types'
+
+const ModalBase = ({ open, onClose, children }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      classes={{
+        root: 'flex justify-center items-center p-5'
+      }}
+      closeAfterTransition
+    >
+      <Grow in={open} timeout={300}>
+        {children}
+      </Grow>
+    </Modal>
+  )
+}
+
+ModalBase.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired
+}
+
+export default ModalBase
---
+++ src/ui/RatingStars.jsx
@@ -0,0 +1,42 @@
+// styling
+import styled from 'styled-components/macro'
+
+// components
+import { ReactComponent as StarEmpty } from '@assets/icons/star-regular.svg'
+import { ReactComponent as StarFilled } from '@assets/icons/star-solid.svg'
+import Rating from '@mui/material/Rating'
+
+// utils
+import PropTypes from 'prop-types'
+
+const StyledRating = styled(Rating)`
+  gap: 15px;
+
+  .MuiRating-iconFilled,
+  .MuiRating-iconEmpty {
+    background: transparent;
+    width: 18px;
+    height: 18px;
+    color: var(--yellow);
+  }
+`
+
+const RatingStars = ({ rating = 0, readOnly = true, ...props }) => {
+  return (
+    <StyledRating
+      readOnly={readOnly}
+      value={rating}
+      precision={0.5}
+      emptyIcon={<StarEmpty />}
+      icon={<StarFilled />}
+      {...props}
+    />
+  )
+}
+
+RatingStars.propTypes = {
+  rating: PropTypes.number,
+  readOnly: PropTypes.bool
+}
+
+export default RatingStars
